08cb22479e08c4348e63f7f2159fa3afe4f82430
Christian Ortega <christianmortega7@gmail.com>
2020-08-14 01:13:43 -0400

    Implement update_display

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,5 +1,18 @@
 class TicTacToe
-  def initialize()
-    board = Array.new(9)
-	end
+  attr_accessor :board
+
+  def initialize
+    @board = (1..9).to_a
+  end
+
+  def update_display
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts '---+---+---'
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts '---+---+---'
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
 end
+
+a = TicTacToe.new
+a.update_display
